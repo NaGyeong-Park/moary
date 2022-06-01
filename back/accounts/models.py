4d28ea279fb2a5a8072de65ee51f35b3a48ad585
NaGyeongPark <ngp.july@gmail.com>
2022-06-01 23:38:08 +0900

    follow add

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     gender = models.BooleanField(blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     private = models.BooleanField(default=False)
